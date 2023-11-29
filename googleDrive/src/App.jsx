29783249dbed26872e42b846f24de9616fc7c211
EfratElimelech2502 <efratelimelech@hilma.tech>
2023-11-29 14:41:03 +0200

    Initial commit

---
+++ googleDrive/src/App.jsx
@@ -0,0 +1,29 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import MyDrive from "./components/MyDrive";
+import Folder from "./components/Folder";
+import File from "./components/File";
+
+function App() {
+  const user = { name: efrat };
+  return (
+    <>
+      <BrowserRouter>
+        {/* <UserProvider> */}
+        <Routes>
+          {/* <Route path="/" element={<Welcome />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<SignUp />} /> */}
+          <Route path="/myDrive">
+            <Route index element={<MyDrive prop />} />
+            <Route path="folder" element={<Folder />} />
+            <Route path="file" element={<File />} />
+          </Route>
+        </Routes>
+        {/* </UserProvider> */}
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ googleDrive/src/components/MyDrive.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+const MyDrive = () => {
+  return (
+    <div>
+      myDrive
+      <Outlet />
+    </div>
+  );
+};
+export default MyDrive;
