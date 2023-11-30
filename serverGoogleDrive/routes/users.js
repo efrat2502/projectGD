fe2a8575e3f973f8ba6f48ef7e7b8a4807029237
EfratElimelech2502 <efratelimelech@hilma.tech>
2023-11-30 11:35:18 +0200

    Revert "my project doesnt work"
    This reverts commit b4d7e49b57caa2f450e49812d4a27dfe781050b1.

--- serverGoogleDrive/routes/users.js
+++ serverGoogleDrive/routes/users.js
@@ -2,14 +2,15 @@ const express = require("express");
 const router = express.Router();
 const users = require("../../DB");
 const showContent = require("../functions/showContent");
-// const fileRoutes = require("./file");
+const fileRoutes = require("./file");
 const path = require("path");
+
+console.log("a: ", users);
 
 router.get("/:id", function (req, res, next) {
   const userId = req.params.id;
   const currUser = users.filter((user) => userId == user.id);
   const url = path.join(__dirname, "../public/efrat");
-  console.log("ccccc");
   // showContent("/efrat", res);
   showContent(url, res);
 
